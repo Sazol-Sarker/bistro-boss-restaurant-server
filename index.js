886f56860b824f2835a5b380cd783c493236ee4b
Sazol Sarker <sazolsarker1@gmail.com>
2025-04-25 17:44:08 +0600

    cart items deleted after sslCommerz payment success

--- index.js
+++ index.js
@@ -644,59 +644,6 @@ Bon appétit!
       res.send(result);
     });
 
-    // SSLCOMMERZE
-    //     Store ID: bistr68074f8368a2d
-    // Store Password (API/Secret Key): bistr68074f8368a2d@ssl
-
-    // Merchant Panel URL: https://sandbox.sslcommerz.com/manage/ (Credential as you inputted in the time of registration)
-
-    // Store name: testbistr24cb
-    // Registered URL: www.bistrobosspro.com
-    // Session API to generate transaction: https://sandbox.sslcommerz.com/gwprocess/v3/api.php
-    // Validation API: https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php?wsdl
-    // Validation API (Web Service) name: https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php
-
-    //sslcommerz init
-    // app.get("/create-ssl-payment", (req, res) => {
-    //   const payment=req.body
-    //   const data = {
-    //     total_amount: 100,
-    //     currency: "BDT",
-    //     tran_id: "REF123", // use unique tran_id for each api call
-    //     success_url: "http://localhost:3030/success",
-    //     fail_url: "http://localhost:3030/fail",
-    //     cancel_url: "http://localhost:3030/cancel",
-    //     ipn_url: "http://localhost:3030/ipn",
-    //     shipping_method: "Courier",
-    //     product_name: "Computer.",
-    //     product_category: "Electronic",
-    //     product_profile: "general",
-    //     cus_name: "Customer Name",
-    //     cus_email: "customer@example.com",
-    //     cus_add1: "Dhaka",
-    //     cus_add2: "Dhaka",
-    //     cus_city: "Dhaka",
-    //     cus_state: "Dhaka",
-    //     cus_postcode: "1000",
-    //     cus_country: "Bangladesh",
-    //     cus_phone: "01711111111",
-    //     cus_fax: "01711111111",
-    //     ship_name: "Customer Name",
-    //     ship_add1: "Dhaka",
-    //     ship_add2: "Dhaka",
-    //     ship_city: "Dhaka",
-    //     ship_state: "Dhaka",
-    //     ship_postcode: 1000,
-    //     ship_country: "Bangladesh",
-    //   };
-    //   const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
-    //   sslcz.init(data).then((apiResponse) => {
-    //     // Redirect the user to payment gateway
-    //     let GatewayPageURL = apiResponse.GatewayPageURL;
-    //     res.redirect(GatewayPageURL);
-    //     console.log("Redirecting to: ", GatewayPageURL);
-    //   });
-    // });
 
     // SSLCommerz
     app.post("/create-ssl-payment", async (req, res) => {
@@ -713,8 +660,8 @@ Bon appétit!
         currency: "BDT",
         tran_id: trxId, // use unique tran_id for each api call
         success_url: "http://localhost:5000/ssl-payment-success",
-        fail_url: "http://localhost:5173/fail",
-        cancel_url: "http://localhost:5173/cancel",
+        fail_url: "http://localhost:5000/fail",
+        cancel_url: "http://localhost:5000/cancel",
         ipn_url: "http://localhost:5000/ssl-ipn-payment",
         shipping_method: "Courier",
         product_name: "Computer.",
@@ -760,6 +707,7 @@ Bon appétit!
       }
     });
 
+    // TODO: clear CART/ delete cart data by email?
     app.post("/ssl-payment-success", async (req, res) => {
       console.log("req.body==>", req.body);
       const { val_id, tran_id, amount, currency } = req.body;
@@ -786,6 +734,14 @@ Bon appétit!
           };
           const paymentResult = await paymentsCollection.updateOne(query,update);
 
+          // clear the cart items
+
+          const paymentDoc = await paymentsCollection.findOne(query);
+          const cartItemDeleteResult=await cartsCollection.deleteMany({userEmail:paymentDoc.email})
+
+          // console.log("paymentDoc==>",paymentDoc);
+
+
 
           res.redirect('http://localhost:5173/payment-success')
 
@@ -807,6 +763,13 @@ Bon appétit!
         res.status(500).send({ message: "SSLCommerz validation error" });
       }
     });
+
+    app.post('/fail',async(req,res)=>{
+      res.redirect('http://localhost:5173/fail')
+    })
+    app.post('/cancel',async(req,res)=>{
+      res.redirect('http://localhost:5173/cancel')
+    })
 
     // **************************
 
